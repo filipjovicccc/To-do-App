297960c310cf106a614a94f50ffe42b9d26fb9f4
filipjovicccc <filipjovicccc@gmail.com>
2021-09-26 18:22:32 +0200

    update

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-function init() {
+
+(function () {
     let btn = document.getElementById("button")
+
     btn.addEventListener("click", addArticle)
-}
-init()
+})()
+
+let input = document.querySelector("input");
+let btn = document.getElementById("button")
+
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        btn.click()
+    }
+})
+
+let dataValue = ""
 
 function addArticle(event) {
 
-    let input = document.querySelector("input");
     let wrapOne = document.getElementById("wrapOne")
 
     const div = document.createElement("div")
 
-    // div.classList.add("innerdiv")
-
-    // wrapOne.innerHTML +=
-    //     `<div id="source">${input.value} <button class="innerButton"></button></div>`  
-
     if (input.value != "") {
-        div.innerHTML += `<div class="source"> <button  class = "secondInnerButton"><i class="fas fa-trash fa-2x"></i> </button> <div class="special">${input.value}</div><button class="innerButton"><i class="fas fa-arrow-circle-right fa-2x"></i> </button></div>`
+        div.innerHTML += `<div class="source"> <button  class =
+         "secondInnerButton"><i class="fas fa-trash fa-2x"></i> 
+        </button> <div class="special"  > ${dataValue = input.value}
+        </div><button class="innerButton">
+        <i class="fas fa-arrow-circle-right fa-2x"></i>
+         </button></div>`
         wrapOne.appendChild(div)
     } else { alert("You cant add empty to do") }
 
 
-
-    // let innerButton = document.getElementById("innerButton")
-    // innerButton.addEventListener("click", arrowNext)
-
     let innerButton = document.querySelectorAll(".innerButton")
-
-
 
     innerButton.forEach(button => {
         button.addEventListener("click", arrowNext)
     })
-
     let secondInnerButton = document.querySelectorAll(".secondInnerButton")
 
     secondInnerButton.forEach(button => {
@@ -44,38 +49,27 @@ function addArticle(event) {
         }
     })
 
-
-
-    // for (var i = 0; i < secondInnerButton.length; i++) {
-    //     secondInnerButton[i].addEventListener('click', function () {
-    //         this.parentNode.remove();
-    //     });
-    // }
-
-
     for (var i = 0; i < innerButton.length; i++) {
         innerButton[i].addEventListener('click', function () {
             this.parentNode.remove();
         });
     }
-
-
 }
 
-
-
 function arrowNext(event) {
-
-
-    let input = document.querySelector("input");
-    let wrapThree = document.getElementById("wrapThree")
 
     let wrapTwo = document.getElementById("wrapTwo")
 
     let divTwo = document.createElement("div")
     wrapTwo.appendChild(divTwo)
 
-    divTwo.innerHTML += `<div class="source"><button class="arrowLeft"><i class="fas fa-arrow-circle-left fa-2x"></i> </button><div class="special"> ${input.value}</div> <button class="innerButton"> <i class="fas fa-arrow-circle-right fa-2x"></i></button></div>`
+    divTwo.innerHTML +=
+        `<div class="source"><button class="arrowLeft"
+    ><i class="fas fa-arrow-circle-left fa-2x"></i> 
+    </button><div class="special"> ${dataValue}</div> 
+    <button class="innerBt"> <i class="fas fa-arrow-circle-right fa-2x">
+    </i></button></div>`
+
 
     let arrowLeft = document.querySelectorAll(".arrowLeft")
 
@@ -89,19 +83,18 @@ function arrowNext(event) {
         button.addEventListener("click", arrowMoove)
     })
 
+    let innerBt = document.querySelectorAll(".innerBt")
+    console.log(innerBt)
 
-    let innerButton = document.querySelectorAll(".innerButton")
 
-
-    innerButton.forEach(button => {
+    innerBt.forEach(button => {
         button.addEventListener("click", arrowSecond)
     })
 
-    for (var i = 0; i < innerButton.length; i++) {
-        innerButton[i].addEventListener('click', function () {
+    for (var i = 0; i < innerBt.length; i++) {
+        innerBt[i].addEventListener('click', function () {
             this.parentNode.remove();
         });
-
     }
 }
 
@@ -109,19 +102,19 @@ function arrowMoove(event) {
 
     let wrapOne = document.getElementById("wrapOne");
 
-
     let divFour = document.createElement("div");
 
     wrapOne.appendChild(divFour)
 
-    let input = document.querySelector("input");
+    divFour.innerHTML +=
+        `<div class="source"> <button class = 
+    "deleteButton"><i class="fas fa-trash fa-2x"></i>
+     </button> <div class="special">${dataValue}
+     </div><button class="innerButton">
+     <i class="fas fa-arrow-circle-right fa-2x">
+     </i></button></div>`
 
-    divFour.innerHTML += `<div class="source"> <button class = "secondInnerButton"><i class="fas fa-trash fa-2x"></i> </button> <div class="special">${input.value}</div><button class="innerButton"><i class="fas fa-arrow-circle-right fa-2x"></i></button></div>`
-
-
-    let secondInnerButton = document.querySelectorAll(".secondInnerButton")
-
-    console.log(secondInnerButton)
+    let deleteButton = document.querySelectorAll(".deleteButton")
 
     let innerButton = document.querySelectorAll(".innerButton")
 
@@ -131,23 +124,16 @@ function arrowMoove(event) {
         });
 
     }
-
-
     innerButton.forEach(button => {
         button.addEventListener("click", arrowNext)
     })
 
 
-
-    for (var i = 0; i < secondInnerButton.length; i++) {
-        secondInnerButton[i].addEventListener('click', function () {
+    for (var i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener('click', function () {
             this.parentNode.remove();
         });
     }
-
-
-
-
 }
 
 function arrowSecond(event) {
@@ -158,30 +144,31 @@ function arrowSecond(event) {
 
     wrapThree.appendChild(divThree)
 
-    let input = document.querySelector("input");
+    // let input = document.querySelector("input");
 
-    divThree.innerHTML += `<div class="source"> <button class = "secondInnerButton"><i class="fas fa-arrow-circle-left fa-2x"></i> </button> <div class="special">${input.value}</div> <button class="innerButton"> <i class="fas fa-trash fa-2x"></i> </button></div>`
+    divThree.innerHTML += `<div class="source"> <button class = 
+    "secondDeleteBtn"><i class="fas fa-arrow-circle-left fa-2x">
+    </i> </button> <div class="special">${dataValue}
+    </div> <button class="innerButton">
+     <i class="fas fa-trash fa-2x"></i>
+      </button></div>`
 
     let innerButton = document.querySelectorAll(".innerButton")
-    let secondInnerButton = document.querySelectorAll(".secondInnerButton")
+    let secondDeleteBtn = document.querySelectorAll(".secondDeleteBtn")
 
-    for (var i = 0; i < secondInnerButton.length; i++) {
-        secondInnerButton[i].addEventListener('click', function () {
+    for (var i = 0; i < secondDeleteBtn.length; i++) {
+        secondDeleteBtn[i].addEventListener('click', function () {
             this.parentNode.remove();
         });
     }
-
-
-
 
     for (var i = 0; i < innerButton.length; i++) {
         innerButton[i].addEventListener('click', function () {
             this.parentNode.remove();
         });
-
     }
 
-    secondInnerButton.forEach(button => {
+    secondDeleteBtn.forEach(button => {
         button.addEventListener("click", arrowMooveAgain)
     })
 
@@ -191,19 +178,17 @@ function arrowMooveAgain(event) {
 
     let wrapTwo = document.getElementById("wrapTwo");
 
-
     let divFive = document.createElement("div");
 
     wrapTwo.appendChild(divFive)
 
-    let input = document.querySelector("input");
-
-    divFive.innerHTML += `<div class="source"><button class = "secondInnerButton"> <i class="fas fa-arrow-circle-left fa-2x"></i></button> <div class="special">${input.value}</div><button class="innerButton"><i class="fas fa-arrow-circle-right fa-2x"></i> </button></div>`
-
+    divFive.innerHTML += `<div class="source"><button class =
+     "secondInnerButton"> <i class="fas fa-arrow-circle-left fa-2x">
+     </i></button> <div class="special">${dataValue}
+     </div><button class="innerButton">
+     <i class="fas fa-arrow-circle-right fa-2x"></i> </button></div>`
 
     let secondInnerButton = document.querySelectorAll(".secondInnerButton")
-
-
 
     let innerButton = document.querySelectorAll(".innerButton")
 
@@ -213,15 +198,9 @@ function arrowMooveAgain(event) {
         });
 
     }
-
-
     innerButton.forEach(button => {
         button.addEventListener("click", arrowNext)
     })
-
-
-
-
 
     for (var i = 0; i < secondInnerButton.length; i++) {
         secondInnerButton[i].addEventListener('click', function () {
